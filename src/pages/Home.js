9be5120e44c37b9cab25678d03497e40ed5acb85
kinshuk-yadav <yadavkinshuk@gmail.com>
2024-04-26 23:15:08 +0530

    homecss update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,36 +3,30 @@ import { useNavigate, Link } from 'react-router-dom';
 import './Home.css'
 import Navbar from '../components/Navbar';
 
+import { GiBrain } from "react-icons/gi";
+
 export default function Home() {
     
   return (
     <div className='home'>
       <Navbar />
       <div className="flex-container">
-        <div className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
-          <div className="card-body">
-            <h5 className="card-title"> Problem of Today</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            
-            {/* <button onClick={() => { (navigate('/Resources')) }} >
-              Read
-            </button> */}
-            <Link to="/profile" className="btn btn-primary">Go somewhere</Link>
-            {/* <Link to="/profile" class="list">
-            Profile
-          </Link> */}
-          </div>
+        <div className="card" >
+          <Link to="/profile" className='CardWrap'>                     
+                <h5 className="card-title" color='BLACK'>PROBLEM OF TODAY <br /> SOLVE NOW !</h5>
+                <GiBrain style={{ fontSize: '150px' }}/><br />  
+          </Link>
         </div>
-        <div className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
-          <div className="card-body">
-            <h5 className="card-title">Feeling Bored?
-            <br />
-              Take a Break...
-            </h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <Link to="/" className="btn btn-primary">Go somewhere</Link>
-          </div>
+
+        <div className="card" >
+          <Link to="/profile" className='CardWrap'>                     
+                <h5 className="card-title">FEELING BORED? <br /> TAKE A BREAK</h5>
+                <GiBrain style={{ fontSize: '150px' }}/><br />  
+          </Link>
         </div>
+
+
+
       </div>
       <div className="flex-container">
         <div className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
