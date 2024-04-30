343e5abdb60fde48533539fa7bab00f1e8c2aa0a
kinshuk-yadav <yadavkinshuk@gmail.com>
2024-05-01 01:35:36 +0530

    final kinshuk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,37 +3,40 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className="navHead">
-        <ul className="navList">
-          <Link to="/" class="list">
-            Home
-          </Link>
-           {/* <Link to="/resources" class="list">
-            Resources
-          </Link>
-          <Link to="/profile" class="list">
-            Profile
-          </Link> */}
-          <Link to="/potd" class="list">
-            Problem of the Day
-          </Link> 
-        </ul>
-      </nav>
+    // <nav className="navHead">
+    //     <ul className="navList">
+    //       <Link to="/home" class="list">
+    //         Home
+    //       </Link>
+    //        {/* <Link to="/resources" class="list">
+    //         Resources
+    //       </Link>
+    //       <Link to="/profile" class="list">
+    //         Profile
+    //       </Link> */}
+    //       <Link to="/potd" class="list">
+    //         Problem of the Day
+    //       </Link> 
+    //     </ul>
+    //   </nav>
 
-        // <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        // <a class="navbar-brand" href="#">Navbar</a>
-        // <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        // <span class="navbar-toggler-icon"></span>
-        // </button>
-        // <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        // <div class="navbar-nav">
-        //     <Link to="/potd" class="nav-item nav-link active" >Home </Link>
-        //     <a class="nav-item nav-link" href="#">Features</a>
-        //     <a class="nav-item nav-link" href="#">Pricing</a>
-        //     <a class="nav-item nav-link disabled" href="#">Disabled</a>
-        // </div>
-        // </div>
-        // </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <div class="container">
+        <div class="navbar-brand mx-auto text-light">JEEHack</div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon text-light"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <Link to="/home" class="nav-item nav-link active text-light" >Home </Link>
+                <Link to="/potd" class="nav-item nav-link active text-light" >Problem Of The Day </Link>
+                <Link to="/potd" class="nav-item nav-link disabled text-dark " >Resources </Link>
+                <Link to="/potd" class="nav-item nav-link disabled text-dark" >Profile </Link>
+            </div>
+        </div>
+    </div>
+</nav>
+
   )
 }
 
